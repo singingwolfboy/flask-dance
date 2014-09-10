0b3bfbbe77b2f703496df690a8b14c0c2ac53981
David Baumgold <david@davidbaumgold.com>
2014-09-10 16:46:00 -0400

    Added license and setup.py

---
+++ setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from setuptools import setup, find_packages
+
+
+def is_requirement(line):
+    line = line.strip()
+    # Skip blank lines, comments, and editable installs
+    return not (
+        line == '' or
+        line.startswith('-r') or
+        line.startswith('#') or
+        line.startswith('-e') or
+        line.startswith('git+')
+    )
+
+
+def get_requirements(path):
+    with open(path) as f:
+        lines = f.readlines()
+    return [l.strip() for l in lines if is_requirement(l)]
+
+
+setup(
+    name="Flask-Dance",
+    version="0.0.1",
+    description="Doing the OAuth dance with style using Flask, requests, and oauthlib",
+    long_description=open('README.rst').read(),
+    author="David Baumgold",
+    author_email="david@davidbaumgold.com",
+    url="https://github.com/singingwolfboy/flask-dance",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+    license='MIT',
+    classifiers=(
+        'License :: OSI Approved :: MIT License',
+        'Framework :: Flask',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ),
+    zip_safe=False,
+)
+
