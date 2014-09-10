15e850a8a802f65ed67478524d1da08338e08d85
David Baumgold <david@davidbaumgold.com>
2014-09-09 22:52:57 -0400

    Added contrib/github.py

---
+++ flask_dance/contrib/github.py
@@ -0,0 +1,15 @@
+from flask_dance.consumer import OAuth2ConsumerBlueprint
+
+
+def make_github_blueprint(client_id, client_secret, scope=None,
+                          redirect_url=None, redirect_to=None):
+    github_bp = OAuth2ConsumerBlueprint("github", __name__,
+        client_id=client_id,
+        client_secret=client_secret,
+        base_url="https://api.github.com",
+        authorization_url="https://github.com/login/oauth/authorize",
+        token_url="https://github.com/login/oauth/access_token",
+        redirect_url=redirect_url,
+        redirect_to=redirect_to,
+    )
+    return github_bp
