6fd64dd76acd4c2bd6a043aca41a24fccef1047d
David Baumgold <david@davidbaumgold.com>
2014-10-04 08:04:09 -0400

    Include logo in latexpdf renderer

--- docs/conf.py
+++ docs/conf.py
@@ -209,7 +209,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+latex_logo = "_static/flask-dance.pdf"
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
