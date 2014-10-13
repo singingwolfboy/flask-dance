f1aace58fd09cf465df7d95615b4066f39d03867
David Baumgold <david@davidbaumgold.com>
2014-10-13 07:31:48 -0400

    version 0.2.2
    bugfix release

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def get_requirements(path):
 
 setup(
     name="Flask-Dance",
-    version="0.2.1",
+    version="0.2.2",
     description="Doing the OAuth dance with style using Flask, requests, and oauthlib",
     long_description=open('README.rst').read(),
     author="David Baumgold",
