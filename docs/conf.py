87e0ecd2e832e40984efb81df140f524801ee0a4
David Baumgold <david@davidbaumgold.com>
2015-03-25 20:38:44 -0400

    Switch back to sphinxcontrib napoleon

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
+    'sphinxcontrib.napoleon',
     'sphinxcontrib.aafig',
 ]
 
