366f621e8cb6fd156a06ce61f46099accd6ab0bc
Chris Keating <christopherkeating@gmail.com>
2015-04-27 00:03:42 -0500

    Pass token in from the blueprint backend.

--- flask_dance/consumer/oauth2.py
+++ flask_dance/consumer/oauth2.py
@@ -142,6 +142,7 @@ class OAuth2ConsumerBlueprint(BaseOAuthConsumerBlueprint):
             auto_refresh_kwargs=self.auto_refresh_kwargs,
             scope=self.scope,
             state=self.state,
+            token=self.token,
             blueprint=self,
             base_url=self.base_url,
             **self.kwargs
