b3d63bc25d6cc5aad825cf0addae664bcfe4c505
David Baumgold <david@davidbaumgold.com>
2015-08-10 22:01:00 -0400

    fix unicode test failure

--- tests/contrib/test_jira.py
+++ tests/contrib/test_jira.py
@@ -96,7 +96,7 @@ def test_content_type(sign_func):
     resp = app.test_client().get("/test")
     headers = responses.calls[0].request.headers
     assert "Content-Type" in headers
-    assert headers["Content-Type"] == "application/json"
+    assert headers["Content-Type"] == "application/json".encode('utf-8')
 
 
 @responses.activate
