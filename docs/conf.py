081f62efd0755c37039c34b84ab928121c1e6545
David Baumgold <david@davidbaumgold.com>
2015-08-16 12:19:50 -0400

    It's 2015

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Flask Dance'
-copyright = u'2014, David Baumgold'
+copyright = u'2014-2015, David Baumgold'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
