9b013ea1b77fb0d3c16ed219bf41775b8b18f9ae
David Baumgold <david@davidbaumgold.com>
2015-12-28 11:48:50 -0500

    update setup.py with 3.5 support

--- setup.py
+++ setup.py
@@ -70,7 +70,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ),
     zip_safe=False,
 )
