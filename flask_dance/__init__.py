ff0938d36c9edcb669acdaa1838ac10612496db9
David Baumgold <david@davidbaumgold.com>
2015-12-28 13:36:53 -0500

    flask_dance.OAuth2ConsumerBlueprint

--- flask_dance/__init__.py
+++ flask_dance/__init__.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 from __future__ import unicode_literals
 
-from .consumer import OAuth1ConsumerBlueprint
+from .consumer import OAuth1ConsumerBlueprint, OAuth2ConsumerBlueprint
 
 __version__ = "0.7.1"
