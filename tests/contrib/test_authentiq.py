1ff7a6bb89805a584ff6c19b3e624981100cb66b
Pieter Ennes <pieter@authentiq.com>
2018-12-19 13:35:33 +0000

    Rename variables.

--- tests/contrib/test_authentiq.py
+++ tests/contrib/test_authentiq.py
@@ -12,36 +12,36 @@ from flask_dance.contrib.authentiq import make_authentiq_blueprint, authentiq
 
 def test_blueprint_factory_default():
     # Test with connect.authentiq.io
-    glbp1 = make_authentiq_blueprint(
+    aqbp1 = make_authentiq_blueprint(
         client_id="foo",
         client_secret="bar",
         scope="openid profile",
         redirect_to="index"
     )
-    assert isinstance(glbp1, OAuth2ConsumerBlueprint)
-    assert glbp1.session.scope == "openid profile"
-    assert glbp1.session.base_url == "https://connect.authentiq.io/"
-    assert glbp1.session.client_id == "foo"
-    assert glbp1.client_secret == "bar"
-    assert glbp1.authorization_url == "https://connect.authentiq.io/authorize"
-    assert glbp1.token_url == "https://connect.authentiq.io/token"
+    assert isinstance(aqbp1, OAuth2ConsumerBlueprint)
+    assert aqbp1.session.scope == "openid profile"
+    assert aqbp1.session.base_url == "https://connect.authentiq.io/"
+    assert aqbp1.session.client_id == "foo"
+    assert aqbp1.client_secret == "bar"
+    assert aqbp1.authorization_url == "https://connect.authentiq.io/authorize"
+    assert aqbp1.token_url == "https://connect.authentiq.io/token"
 
 
 def test_blueprint_factory_custom():
-    glbp2 = make_authentiq_blueprint(
+    aqbp2 = make_authentiq_blueprint(
         client_id="foo",
         client_secret="bar",
         scope="openid profile",
         redirect_to="index",
         hostname="local.example.com"
     )
-    assert isinstance(glbp2, OAuth2ConsumerBlueprint)
-    assert glbp2.session.scope == "openid profile"
-    assert glbp2.session.base_url == "https://local.example.com/"
-    assert glbp2.session.client_id == "foo"
-    assert glbp2.client_secret == "bar"
-    assert glbp2.authorization_url == "https://local.example.com/authorize"
-    assert glbp2.token_url == "https://local.example.com/token"
+    assert isinstance(aqbp2, OAuth2ConsumerBlueprint)
+    assert aqbp2.session.scope == "openid profile"
+    assert aqbp2.session.base_url == "https://local.example.com/"
+    assert aqbp2.session.client_id == "foo"
+    assert aqbp2.client_secret == "bar"
+    assert aqbp2.authorization_url == "https://local.example.com/authorize"
+    assert aqbp2.token_url == "https://local.example.com/token"
 
 
 def test_load_from_config():
@@ -64,18 +64,18 @@ def test_context_local():
 
     # set up two apps with two different set of auth tokens
     app1 = Flask(__name__)
-    glbp1 = make_authentiq_blueprint(
+    aqbp1 = make_authentiq_blueprint(
         "foo1", "bar1", redirect_to="url1",
         backend=MemoryBackend({"access_token": "app1"}),
     )
-    app1.register_blueprint(glbp1)
+    app1.register_blueprint(aqbp1)
 
     app2 = Flask(__name__)
-    glbp2 = make_authentiq_blueprint(
+    aqbp2 = make_authentiq_blueprint(
         "foo2", "bar2", redirect_to="url2",
         backend=MemoryBackend({"access_token": "app2"}),
     )
-    app2.register_blueprint(glbp2)
+    app2.register_blueprint(aqbp2)
 
     # outside of a request context, referencing functions on the `authentiq`
     # object will raise an exception
