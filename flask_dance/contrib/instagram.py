2396e8ec5bd84bbf2aa052f694a9ea97f7190e61
Adam Goldschmidt <adamgold7@gmail.com>
2019-12-30 21:35:50 +0200

    change backend to storage

--- flask_dance/contrib/instagram.py
+++ flask_dance/contrib/instagram.py
@@ -4,6 +4,7 @@ from flask_dance.consumer import OAuth2ConsumerBlueprint
 from requests_oauthlib.compliance_fixes.instagram import instagram_compliance_fix
 from functools import partial
 from flask.globals import LocalProxy, _lookup_app_object
+
 try:
     from flask import _app_ctx_stack as stack
 except ImportError:
@@ -19,9 +20,16 @@ class InstagramBlueprint(OAuth2ConsumerBlueprint):
 
 
 def make_instagram_blueprint(
-        client_id=None, client_secret=None, scope=None, redirect_url=None,
-        redirect_to=None, login_url=None, authorized_url=None,
-        session_class=None, backend=None):
+    client_id=None,
+    client_secret=None,
+    scope=None,
+    redirect_url=None,
+    redirect_to=None,
+    login_url=None,
+    authorized_url=None,
+    session_class=None,
+    storage=None,
+):
     """
     Make a blueprint for authenticating with Instagram using OAuth 2. This requires
     a client ID and client secret from Instagram. You should either pass them to
@@ -44,30 +52,31 @@ def make_instagram_blueprint(
         session_class (class, optional): The class to use for creating a
             Requests session. Defaults to
             :class:`~flask_dance.consumer.requests.OAuth2Session`.
-        backend: A storage backend class, or an instance of a storage
-                backend class, to use for this blueprint. Defaults to
-                :class:`~flask_dance.consumer.backend.session.SessionBackend`.
+        storage: A token storage class, or an instance of a token storage
+                class, to use for this blueprint. Defaults to
+                :class:`~flask_dance.consumer.storage.session.SessionStorage`.
 
     :rtype: :class:`~flask_dance.consumer.OAuth2ConsumerBlueprint`
     :returns: A :ref:`blueprint <flask:blueprints>` to attach to your Flask app.
     """
     scope = scope or ["identify", "chat:write:bot"]
-    instagram_bp = InstagramBlueprint("instagram", __name__,
-                                      client_id=client_id,
-                                      client_secret=client_secret,
-                                      scope=scope,
-                                      base_url="https://api.instagram.com/v1/",
-                                      authorization_url="https://api.instagram.com/oauth/authorize/",
-                                      token_url="https://api.instagram.com/oauth/access_token",
-                                      redirect_url=redirect_url,
-                                      redirect_to=redirect_to,
-                                      login_url=login_url,
-                                      authorized_url=authorized_url,
-                                      session_class=session_class,
-                                      backend=backend,
-                                      token_url_params={
-                                          'include_client_id': True},
-                                      )
+    instagram_bp = InstagramBlueprint(
+        "instagram",
+        __name__,
+        client_id=client_id,
+        client_secret=client_secret,
+        scope=scope,
+        base_url="https://api.instagram.com/v1/",
+        authorization_url="https://api.instagram.com/oauth/authorize/",
+        token_url="https://api.instagram.com/oauth/access_token",
+        redirect_url=redirect_url,
+        redirect_to=redirect_to,
+        login_url=login_url,
+        authorized_url=authorized_url,
+        session_class=session_class,
+        storage=storage,
+        token_url_params={"include_client_id": True},
+    )
     instagram_bp.from_config["client_id"] = "INSTAGRAM_OAUTH_CLIENT_ID"
     instagram_bp.from_config["client_secret"] = "INSTAGRAM_OAUTH_CLIENT_SECRET"
 
