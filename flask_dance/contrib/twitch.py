680c0a753f471464ae75409ed8d21f8df7662a73
Kerry Hatcher <kerry.hatcher@protonmail.com>
2021-04-07 00:38:01 -0400

    Refactor with PR review sugestions
    See: https://github.com/singingwolfboy/flask-dance/pull/348#discussion_r607773457

--- flask_dance/contrib/twitch.py
+++ flask_dance/contrib/twitch.py
@@ -1,3 +1,5 @@
+"""Blueprint for connecting to Twitch API."""
+
 from flask_dance.consumer import OAuth2ConsumerBlueprint
 from functools import partial
 from flask.globals import LocalProxy, _lookup_app_object
@@ -8,15 +10,8 @@ from flask import _app_ctx_stack as stack
 
 __maintainer__ = "Kerry Hatcher <kerry.hatcher@protonmail.com>"
 
-
-class TwitchBlueprint(OAuth2ConsumerBlueprint):
-    """
-    Add client-id to the API request headers
-    """
-
-    def session_created(self, session):
-        session.headers.update({"client-id": os.getenv("TWITCH_CLIENTID")})
-        return session
+class MissingConfigException(Exception):
+    """Exception raised when a required config is missing."""
 
 
 def make_twitch_blueprint(
@@ -26,49 +21,48 @@ def make_twitch_blueprint(
     redirect_url=None,
     redirect_to=None,
     login_url=None,
+    base_url="https://api.twitch.tv/helix/",
+    authorization_url="https://id.twitch.tv/oauth2/authorize",
+    token_url="https://id.twitch.tv/oauth2/token",
+    token_url_params={"include_client_id": True},
     authorized_url=None,
     session_class=None,
-    storage=None,
-):
-    """
-    Make a blueprint for authenticating with Twitch using OAuth 2. This requires
-    a client ID and client secret from Twitch. You should either pass them to
-    this constructor, or make sure that your Flask application config defines
-    them, using the variables :envvar:`TWITCH_OAUTH_CLIENT_ID` and
-    :envvar:`TWITCH_OAUTH_CLIENT_SECRET`.
-    Args:
-        client_id (str): The client ID for your application on Twitch
-        client_secret (str): The client secret for your application on Twitch
-        scope (str, optional): comma-separated list of scopes for the OAuth token
-        redirect_url (str): the URL to redirect to after the authentication
-            dance is complete
-        redirect_to (str): if ``redirect_url`` is not defined, the name of the
-            view to redirect to after the authentication dance is complete.
-            The actual URL will be determined by :func:`flask.url_for`
-        login_url (str, optional): the URL path for the ``login`` view.
-            Defaults to ``/twitch``
-        authorized_url (str, optional): the URL path for the ``authorized`` view.
-            Defaults to ``/twitch/authorized``.
-        session_class (class, optional): The class to use for creating a
-            Requests session. Defaults to
-            :class:`~flask_dance.consumer.requests.OAuth2Session`.
-        storage: A token storage class, or an instance of a token storage
-                class, to use for this blueprint. Defaults to
-                :class:`~flask_dance.consumer.storage.session.SessionStorage`.
-    :rtype: :class:`~flask_dance.consumer.OAuth2ConsumerBlueprint`
-    :returns: A :ref:`blueprint <flask:blueprints>` to attach to your Flask app.
-    """
+    storage=None, ):
+    """[summary]
 
-    twitch_bp = TwitchBlueprint(
+    Args:
+        client_id ([type], optional): [description]. Defaults to None.
+        client_secret ([type], optional): [description]. Defaults to None.
+        scope ([type], optional): [description]. Defaults to None.
+        redirect_url ([type], optional): [description]. Defaults to None.
+        redirect_to ([type], optional): [description]. Defaults to None.
+        login_url ([type], optional): [description]. Defaults to None.
+        base_url (str, optional): [description]. Defaults to "https://api.twitch.tv/helix/".
+        authorization_url (str, optional): [description]. Defaults to "https://id.twitch.tv/oauth2/authorize".
+        token_url (str, optional): [description]. Defaults to "https://id.twitch.tv/oauth2/token".
+        token_url_params (dict, optional): [description]. Defaults to {"include_client_id": True}.
+        authorized_url ([type], optional): [description]. Defaults to authorized_url.
+        session_class ([type], optional): [description]. Defaults to None.
+        storage ([type], optional): [description]. Defaults to None.
+
+    Raises:
+        TypeError: [description]
+        TypeError: [description]
+        TypeError: [description]
+
+    Returns:
+        [type]: [description]
+    """
+    twitch_bp = OAuth2ConsumerBlueprint(
         "twitch",
         __name__,
         client_id=client_id,
         client_secret=client_secret,
         scope=scope,
-        base_url="https://api.twitch.tv/helix/",
-        authorization_url="https://id.twitch.tv/oauth2/authorize",
-        token_url="https://id.twitch.tv/oauth2/token",
-        token_url_params={"include_client_id": True},
+        base_url=base_url,
+        authorization_url=authorization_url,
+        token_url=token_url,
+        token_url_params=token_url_params,
         redirect_url=redirect_url,
         redirect_to=redirect_to,
         login_url=login_url,
@@ -76,12 +70,35 @@ def make_twitch_blueprint(
         session_class=session_class,
         storage=storage,
     )
-    twitch_bp.from_config["client_id"] = "TWITCH_OAUTH_CLIENT_ID"
-    twitch_bp.from_config["client_secret"] = "TWITCH_OAUTH_CLIENT_SECRET"
+    if twitch_bp.client_id is None:
+        twitch_bp.from_config["client_id"] = "TWITCH_OAUTH_CLIENT_ID"
+    
+
+    if twitch_bp.client_secret is None:
+        twitch_bp.from_config["client_secret"] = "TWITCH_OAUTH_CLIENT_SECRET"
+    
+
+    
 
     @twitch_bp.before_app_request
     def set_applocal_session():
+
         ctx = stack.top
+
+       # if twitch_bp.client_id is None:
+       #     raise TypeError(f"client_id is required, currently is {type(twitch_bp.client_id)}")
+
+        if type(twitch_bp.client_id) != str:
+            raise MissingConfigException(f"client_id should be a str, is: {type(twitch_bp.client_id)}")
+
+       # if twitch_bp.client_id is None:
+       #     raise TypeError(f"client_id is required, currently is {type(twitch_bp.client_id)}")
+
+        if type(twitch_bp.client_secret) != str:
+            raise MissingConfigException(f"client_secret should be a str, is {type(twitch_bp.client_secret)}")
+    
+
+        twitch_bp.session.headers.update({"client-id": twitch_bp.client_id})
 
         ctx.twitch_oauth = twitch_bp.session
 
