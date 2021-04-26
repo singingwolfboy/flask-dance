c1536110677b2704d23b413775999bcba3833239
Baire Adams <github.spacebaire@xoxy.net>
2021-04-26 23:22:51 +0200

    #357 Ensure there are 2 rules that match

--- tests/contrib/test_jira.py
+++ tests/contrib/test_jira.py
@@ -54,16 +54,18 @@ def test_blueprint_factory_rule_kwargs(make_app):
         redirect_to="index",
         rule_kwargs={"host":"example2.com"},
     )
-    rules = [rule for rule in app.url_map.iter_rules() if rule.endpoint.startswith("google.")]
+    rules = [rule for rule in app.url_map.iter_rules() if rule.endpoint.startswith("jira.")]
     assert all(rule.host == "example2.com" for rule in rules)
+    assert len(rules) == 2
 
 
 def test_blueprint_factory_no_rule_kwargs(make_app):
     app = make_app("https://flask.atlassian.net",
         redirect_to="index",
     )
-    rules = [rule for rule in app.url_map.iter_rules() if rule.endpoint.startswith("google.")]
+    rules = [rule for rule in app.url_map.iter_rules() if rule.endpoint.startswith("jira.")]
     assert all(rule.host is None for rule in rules)
+    assert len(rules) == 2
 
 
 def test_rsa_key_file(tmp_path):
