e615666127b6cd4efc412f3bd305ebd8966b6af5
David Baumgold <david@davidbaumgold.com>
2021-05-05 13:04:26 +0200

    rules_kwargs for twitch

--- flask_dance/contrib/twitch.py
+++ flask_dance/contrib/twitch.py
@@ -32,6 +32,7 @@ def make_twitch_blueprint(
     authorized_url=None,
     session_class=ClientIdHeaderOAuth2Session,
     storage=None,
+    rule_kwargs=None,
 ):
     """Make a blueprint for authenticating with Twitch using OAuth 2.
 
@@ -62,6 +63,8 @@ def make_twitch_blueprint(
         storage: A token storage class, or an instance of a token storage
                 class, to use for this blueprint. Defaults to
                 :class:`~flask_dance.consumer.storage.session.SessionStorage`.
+        rule_kwargs (dict, optional): Additional arguments that should be passed when adding
+            the login and authorized routes. Defaults to ``None``.
 
     Returns:
         :class:`~flask_dance.consumer.OAuth2ConsumerBlueprint` A :ref:`blueprint <flask:blueprints>` to attach to your Flask app.
@@ -82,6 +85,7 @@ def make_twitch_blueprint(
         authorized_url=authorized_url,
         session_class=session_class,
         storage=storage,
+        rule_kwargs=rule_kwargs,
     )
 
     twitch_bp.from_config["client_id"] = "TWITCH_OAUTH_CLIENT_ID"
